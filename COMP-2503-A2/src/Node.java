b34d00cf043428d2f309ddc1373d7c7b69508d38
mfudg395 <mfudg395@B107-009.ad.mtroyal.ca>
2020-02-25 14:44:51 -0700

    Implemented Node class

--- COMP-2503-A2/src/Node.java
+++ COMP-2503-A2/src/Node.java
@@ -1,4 +1,33 @@
+public class Node<T> {
+   
+    private T data;
+    private Node<T> next;
+    
+    /**
+     * Constructor for objects of class Node
+     */
+    public Node(T d) {
+       data = d;
+       next = null;
+    }
 
-public class Node {
-
+    public T getData() { 
+    	return data; 
+    }
+    
+    public void setData(T o) { 
+    	data = o; 
+    }
+    
+    public Node<T> getNext() { 
+    	return next; 
+    }
+    
+    public void setNext(Node<T> n) { 
+    	next = n; 
+    }
+    
+    public String toString() {
+        return "Node: " + getData().toString();
+    }    
 }
